a371a0260ed26604a0a4f020745337333457c2ba
Yang Liu <byliuyang11@gmail.com>
2016-12-28 22:24:18 -0500

    Removed course component

--- src/app/schools/courses/courses-routing.module.ts
+++ src/app/schools/courses/courses-routing.module.ts
@@ -1,14 +1,9 @@
 import {Routes, RouterModule} from "@angular/router";
-import {CoursesComponent} from "./courses.component";
 import {NgModule} from "@angular/core";
 /**
  * Created by harryliu on 12/25/16.
  */
 const courseRoutes: Routes = [
-  {
-    path: 'schools/:school_id/courses',
-    component: CoursesComponent
-  }
 ];
 
 @NgModule({
--- src/app/schools/courses/courses.module.ts
+++ src/app/schools/courses/courses.module.ts
@@ -1,15 +1,12 @@
-import {CoursesComponent} from "./courses.component";
 import {NgModule} from "@angular/core";
 import {CoursesRoutingModule} from "./courses-routing.module";
 import { CourseListComponent } from './course-list/course-list.component';
-import {ProblemListComponent} from "./problems/problem-list/problem-list.component";
 import {SharedModule} from "../../shared/shared.module";
 /**
  * Created by harryliu on 12/25/16.
  */
 @NgModule({
   declarations: [
-    CoursesComponent,
     CourseListComponent
   ],
   imports: [
