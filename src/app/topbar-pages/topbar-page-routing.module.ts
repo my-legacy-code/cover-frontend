1792845fb6a622b9b9d25981a5337750414aa17e
Yang Liu <byliuyang11@gmail.com>
2017-02-13 22:11:28 -0500

    Add register link

--- src/app/topbar-pages/topbar-page-routing.module.ts
+++ src/app/topbar-pages/topbar-page-routing.module.ts
@@ -8,7 +8,8 @@ import {TopbarPageComponent} from "./topbar-page.component";
 const topbarPageRoutes: Routes = [
   {path: '',  component: TopbarPageComponent, children: [
     {path: 'schools', loadChildren: 'app/topbar-pages/schools/schools.module#SchoolsModule'},
-    {path: 'new', loadChildren: 'app/topbar-pages/new/new.module#NewModule'}
+    {path: 'new', loadChildren: 'app/topbar-pages/new/new.module#NewModule'},
+    {path: '', loadChildren: 'app/topbar-pages/authentication/authentication.module#AuthenticationModule'}
   ]}
 
 ];
